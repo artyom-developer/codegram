0c49e9ae40b3315dda1adbfe59f1cc54236726b5
artyom-developer <felipeperdomo32@hotmail.com>
2022-06-08 20:38:58 -0500

    helmet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,22 @@ import React, { Fragment } from 'react'
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../containers/ListOfPhotoCards'
 
+import { Layout } from '../components/Layout'
+
 export const Home = ({ categoryId }) => {
   return (
-    <Fragment>
+    <Layout title='Tu app de fotos de mascotas' subtitle='Con Petgram puedes encontrar fotos de animales domésticos muy bonitos'>
       <ListOfCategories />
       <ListOfPhotoCards categoryId={categoryId} />
-    </Fragment>
+    </Layout>
   )
 }
+
+// export const Home = ({ categoryId }) => {
+//   return (
+//     <Fragment>
+//       <ListOfCategories />
+//       <ListOfPhotoCards categoryId={categoryId} />
+//     </Fragment>
+//   )
+// }
